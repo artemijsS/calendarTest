8074a9e628af7a18686f488d94209d539f7a72b4
artemijsS <artjom1408@gmail.com>
2023-06-01 15:36:31 +0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import '@/styles/reset.css'
 import '@/styles/globals.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
